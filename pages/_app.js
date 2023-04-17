e6f5bda3103cb45849e83e4b209ad390bb999fd1
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2023-04-17 18:13:22 +0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 import '../styles/globals.css'
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+import Feature from "../components/Feature";
+import Grid from "../components/Grid";
+import Page from "../components/Page";
+import Teaser from "../components/Teaser";
+ 
+const components = {
+  feature: Feature,
+  grid: Grid,
+  teaser: Teaser,
+  page: Page,
+};
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+ 
+storyblokInit({
+  accessToken: "gxeC8W3nEA8Wh5gIS3iyzgtt",
+  use: [apiPlugin],
+  components,
+
+});
+ 
+
 
 export default MyApp
